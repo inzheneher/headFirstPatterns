41f6ad5c1d16a04411b8dc84a4fc796a014b48d6
inzheneher <inzheneher@ya.ru>
2019-04-07 02:13:58 +0300

    add two realisations of Duck

---
+++ src/main/java/strategy/MallardDuck.java
@@ -0,0 +1,23 @@
+package strategy;
+
+public class MallardDuck extends Duck {
+    @Override
+    String quack() {
+        return null;
+    }
+
+    @Override
+    String swim() {
+        return null;
+    }
+
+    @Override
+    String display() {
+        return null;
+    }
+
+    @Override
+    String fly() {
+        return null;
+    }
+}
---
+++ src/main/java/strategy/RedheadDuck.java
@@ -0,0 +1,23 @@
+package strategy;
+
+public class RedheadDuck extends Duck {
+    @Override
+    String quack() {
+        return null;
+    }
+
+    @Override
+    String swim() {
+        return null;
+    }
+
+    @Override
+    String display() {
+        return null;
+    }
+
+    @Override
+    String fly() {
+        return null;
+    }
+}
