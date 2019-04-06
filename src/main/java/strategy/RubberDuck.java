7a491dd196fddc01f5efd6661ff323437af190c9
inzheneher <inzheneher@ya.ru>
2019-04-07 02:29:55 +0300

    add RubberDuck

---
+++ src/main/java/strategy/RubberDuck.java
@@ -0,0 +1,23 @@
+package strategy;
+
+public class RubberDuck extends Duck {
+    @Override
+    String quack() {
+        return null;
+    }
+
+    @Override
+    String swim() {
+        return null;
+    }
+
+    @Override
+    String display() {
+        return null;
+    }
+
+    @Override
+    String fly() {
+        return null;
+    }
+}
